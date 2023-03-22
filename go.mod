25dd46376d183bf518ced93a3eb405d0f21b82b4
David Case <david@fyxgaming.com>
2023-03-22 03:14:22 -0400

    Add server code

---
+++ go.mod
@@ -0,0 +1,53 @@
+module github.com/shruggr/1sat-server
+
+go 1.20
+
+require (
+	github.com/gin-gonic/gin v1.9.0
+	github.com/joho/godotenv v1.5.1
+	github.com/libsv/go-bt v1.0.8
+	github.com/libsv/go-bt/v2 v2.1.1
+	github.com/shruggr/1sat-indexer v0.0.0-20230322071255-69e59ad88d7b
+)
+
+require (
+	github.com/GorillaPool/go-junglebus v0.0.0-20220928133317-2e3624f72bf9 // indirect
+	github.com/bitcoinsv/bsvd v0.0.0-20190609155523-4c29707f7173 // indirect
+	github.com/bitcoinsv/bsvutil v0.0.0-20181216182056-1d77cf353ea9 // indirect
+	github.com/bytedance/sonic v1.8.5 // indirect
+	github.com/centrifugal/centrifuge-go v0.9.4 // indirect
+	github.com/centrifugal/protocol v0.9.1 // indirect
+	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.12.0 // indirect
+	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/hashicorp/golang-lru/v2 v2.0.2 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/jpillora/backoff v1.0.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
+	github.com/leodido/go-urn v1.2.2 // indirect
+	github.com/lib/pq v1.10.7 // indirect
+	github.com/libsv/go-bk v0.1.6 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.7 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/segmentio/asm v1.2.0 // indirect
+	github.com/segmentio/encoding v0.3.6 // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	github.com/ugorji/go/codec v1.2.11 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	golang.org/x/arch v0.3.0 // indirect
+	golang.org/x/crypto v0.7.0 // indirect
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"crypto/sha256"
+	"database/sql"
+	"encoding/hex"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/libsv/go-bt"
+	"github.com/libsv/go-bt/v2/bscript"
+	"github.com/shruggr/1sat-indexer/lib"
+	// _ "github.com/shruggr/bsv-ord-indexer/server/docs"
+	// swaggerFiles "github.com/swaggo/files"
+	// ginSwagger "github.com/swaggo/gin-swagger"
+)
+
+var db *sql.DB
+
+func init() {
+	godotenv.Load()
+
+	var err error
+	db, err = sql.Open("postgres", os.Getenv("POSTGRES"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = lib.Initialize(db)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	r := gin.Default()
+	// url := ginSwagger.URL("/swagger/doc.json")
+	// r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, url))
+
+	r.GET("/api/utxos/address/:address", func(c *gin.Context) {
+		script, err := bscript.NewP2PKHFromAddress(c.Param("address"))
+		if err != nil {
+			handleError(c, &lib.HttpError{
+				StatusCode: http.StatusBadRequest,
+				Err:        err,
+			})
+		}
+		hash := sha256.Sum256(*script)
+		lock := bt.ReverseBytes(hash[:])
+		utxos, err := lib.LoadUtxos(lock)
+		if err != nil {
+			handleError(c, err)
+		}
+
+		c.Header("cache-control", "no-cache,no-store,must-revalidate")
+		c.JSON(http.StatusOK, utxos)
+	})
+
+	r.GET("/api/utxos/lock/:lock", func(c *gin.Context) {
+		lock, err := hex.DecodeString(c.Param("lock"))
+		if err != nil {
+			handleError(c, &lib.HttpError{
+				StatusCode: http.StatusBadRequest,
+				Err:        err,
+			})
+		}
+		utxos, err := lib.LoadUtxos(lock)
+		if err != nil {
+			handleError(c, err)
+		}
+
+		c.Header("cache-control", "no-cache,no-store,must-revalidate")
+		c.JSON(http.StatusOK, utxos)
+	})
+
+	r.GET("/api/inscriptions/:origin", func(c *gin.Context) {
+		origin, err := lib.NewOriginFromString(c.Param("origin"))
+		if err != nil {
+			handleError(c, err)
+			return
+		}
+		im, err := lib.LoadInscriptions(origin)
+		if err != nil {
+			handleError(c, err)
+			return
+		}
+		c.Header("cache-control", "max-age=604800,immutable")
+		c.JSON(http.StatusOK, im)
+	})
+
+	r.GET("/api/files/inscriptions/:origin", func(c *gin.Context) {
+		origin, err := lib.NewOriginFromString(c.Param("origin"))
+		if err != nil {
+			handleError(c, err)
+			return
+		}
+		ins, err := lib.LoadInscriptionFile(origin)
+		if err != nil {
+			handleError(c, err)
+			return
+		}
+		c.Header("cache-control", "max-age=604800,immutable")
+		c.Data(http.StatusOK, ins.Type, ins.Body)
+	})
+
+	listen := os.Getenv("LISTEN")
+	if listen == "" {
+		listen = "0.0.0.0:8080"
+	}
+	r.Run(listen) // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+func handleError(c *gin.Context, err error) {
+	if httpErr, ok := err.(*lib.HttpError); ok {
+		c.String(httpErr.StatusCode, "%v", httpErr.Err)
+	} else {
+		c.String(http.StatusInternalServerError, "%v", err)
+	}
+}
